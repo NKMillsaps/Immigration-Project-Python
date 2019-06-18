6e997a52540f159015f790c2427977fd2eb7e61c
NKMillsaps <nkmillsaps@gmail.com>
2019-06-18 01:26:18 +0000

    parent-child buttle

--- src/models.py
+++ src/models.py
@@ -2,31 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class Person(db.Model):
     __tablename__ = "person"
-    spouse = relationship("Spouse", uselist=False, back_populates="person")
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    lastname = db.Column(db.String(120), unique=False, nullable=True)
-    firstname = db.Column(db.String(120), unique=False, nullable=True)
-    middlename = db.Column(db.String(120), unique=False, nullable=True)
-    sex = db.Column(db.String(6), unique=False, nullable=True)
-    dateOfBirth = db.Column(db.Date, unique=False, nullable=True)
-    countyOfBirth = db.Column(db.String(30), unique=False, nullable=True)
-    citizenship = db.Column(db.String(30), unique=False, nullable=True)
-    US_Address = db.Column(db.String(120), unique=False, nullable=True)
-    apartment = db.Column(db.String(120), unique=False, nullable=True)
-    city = db.Column(db.String(30), unique=False, nullable=True)
-    state = db.Column(db.String(20), unique=False, nullable=True)
-    zip = db.Column(db.String(10), unique=False, nullable=True)
-    mobile = db.Column(db.String(20), unique=False, nullable=True)
-
+spouse = relationship("Spouse", uselist=False, back_populates="person")
+id = db.Column(db.Integer, primary_key=True)
+person_id = db.Column(db.Integer, unique=True, nullable=False)
+username = db.Column(db.String(80), unique=True, nullable=False)
+email = db.Column(db.String(120), unique=True, nullable=False)
+lastname = db.Column(db.String(120), unique=False, nullable=True)
+firstname = db.Column(db.String(120), unique=False, nullable=True)
+middlename = db.Column(db.String(120), unique=False, nullable=True)
+sex = db.Column(db.String(6), unique=False, nullable=True)
+dateOfBirth = db.Column(db.Date, unique=False, nullable=True)
+countyOfBirth = db.Column(db.String(30), unique=False, nullable=True)
+citizenship = db.Column(db.String(30), unique=False, nullable=True)
+US_Address = db.Column(db.String(120), unique=False, nullable=True)
+apartment = db.Column(db.String(120), unique=False, nullable=True)
+city = db.Column(db.String(30), unique=False, nullable=True)
+state = db.Column(db.String(20), unique=False, nullable=True)
+zip = db.Column(db.String(10), unique=False, nullable=True)
+mobile = db.Column(db.String(20), unique=False, nullable=True)
 
 
 
     def __repr__(self):
-        return '<Person %r>' % self.username
+        return '<Person %r>' % self.person_id
 
     def serialize(self):
         return {
@@ -45,17 +46,18 @@ class Person(db.Model):
             "state": self.state,
             "zip": self.zip,
             "mobile": self.mobile,
+            "person_id": self.person_id,
             "spouse_id": self.spouse_id,
             "application_id": self.application_id
-
 
         }
 
     class Spouse(db.Model):
         __tablename__ = 'spouse'
-        person_id = Column(Integer, ForeignKey('person.id'))
-        person = relationship("Person", back_populates="spouse")
+        person_id = db.Column(db.Integer, ForeignKey('person.id'))
+        person = db.relationship("Person", back_populates="spouse")
         id = db.Column(db.Integer, primary_key=True)
+
         email = db.Column(db.String(120), unique=True, nullable=False)
         lastname = db.Column(db.String(120), unique=False, nullable=True)
         firstname = db.Column(db.String(120), unique=False, nullable=True)
@@ -71,6 +73,7 @@ class Person(db.Model):
         state = db.Column(db.String(20), unique=False, nullable=True)
         zip = db.Column(db.String(10), unique=False, nullable=True)
         mobile = db.Column(db.String(20), unique=False, nullable=True)
+        spouse_id = db.Column(db.Integer(20), unique=True, nullable=True)
 
 
 
@@ -102,9 +105,9 @@ class Person(db.Model):
     class Application(db.Model):
         __tablename__ = 'application'
         id = Column(Integer, primary_key=True)
-        id = relationship("Forms")
+        children = relationship("Forms")
         application_name = db.Column(db.String(80), unique=True, nullable=False)
-        form_id = db.Column(db.String(120), unique=True, nullable=False)
+        application_id = db.Column(db.String(120), unique=True, nullable=False)
 
 
     def __repr__(self):
@@ -114,16 +117,14 @@ class Person(db.Model):
         return {
             "form_done": self.form_done,
             "application_id": self.application_id
-
-
         }
 
 
     class Forms(db.Model):
         __tablename__ = "forms"
         id = Column(Integer, primary_key=True)
-        application_id = Column(Integer, ForeignKey('application.id'))
-
+        application_id = Column(Integer, ForeignKey("application.id"))
+        forms_id = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Forms %r>' % self.email
@@ -131,8 +132,7 @@ class Person(db.Model):
     def serialize(self):
         return {
             "email": self.email,
-
-            "application_id": self.application_id
+            "forms_id": self.forms_id
 
 
         }
