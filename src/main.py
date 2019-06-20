a68ad491127acdfadf2d2e2c8399c0b41af96b42
NKMillsaps <nkmillsaps@gmail.com>
2019-06-20 22:06:11 +0000

    news thurdatas

--- src/main.py
+++ src/main.py
@@ -7,10 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Person
-from models import db, Spouse
-from models import db, Application
-from models import db, Forms
+from models import db, Person, Spouse, Application, Forms
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -46,31 +43,33 @@ def handle_person():
         if  "lastname" not in body:
             raise APIException("You need to specify Last Name", status_code=400)
         if "firstname" not in body:
-            raise APIException("You need to specify the First Name")
+            raise APIException("You need to specify the First Name", status_code=400)
         if "middlename" not in body:
-            raise APIException("You need to specify the Middle Name or N/A")
+            raise APIException("You need to specify the Middle Name or N/A", status_code=400)
         if "sex" not in body:
-            raise APIException("You need to specify sex")
+            raise APIException("You need to specify gender", status_code=400)
         if "dateOfBirth" not in body:
-            raise APIException("You need to specify your date of birth")
-        if "countyOfBirth" not in body:
-            raise APIException("You need to specify your country of birth or N/A")
+            raise APIException("You need to specify your date of birth", status_code=400)
+        if "countryOfBirth" not in body:
+            raise APIException("You need to specify your country of birth or N/A", status_code=400)
         if "citizenship" not in body:
-            raise APIException("You need to specify your citizenship or N/A")
+            raise APIException("You need to specify your citizenship or N/A", status_code=400)
         if "US_Address" not in body:
-            raise APIException("You need to specify your address")
+            raise APIException("You need to specify your address", status_code=400)
         if "city" not in body:
-            raise APIException("You need to specify the city")
+            raise APIException("You need to specify the city", status_code=400)
         if "state" not in body:
-            raise APIException("You need to specify state")
+            raise APIException("You need to specify state", status_code=400)
         if "zip" not in body:
-            raise APIException("You need to specify zip code")
+            raise APIException("You need to specify zip code", status_code=400)
+        if "mobile" not in body:
+            raise APIException("You need to specify your phone number or N/A", status_code=400)
 
+    user1 = Person(username=body["username"], email=body["email"], lastname=body["lastname"], firstname=body["firstname"], middlename=body["middlename"], sex=body["sex"], dateOfBirth=body["dateOfBirth"], countryOfBirth=body["countryOfBirth"], citizenship=body["citizenship"], US_Address=body["US_Address"], city=body["city"], state=body["state"], zip=body["zip"], mobile=body["mobile"])
 
-        user1 = Person(username=body['username'], email=body['email'])
-        db.session.add(user1)
-        db.session.commit()
-        return "ok", 200
+    db.session.add(user1)
+    db.session.commit()
+    return "ok", 200
 
     # GET request
     if request.method == 'GET':
@@ -101,6 +100,32 @@ def get_single_person(person_id):
             user1.username = body["username"]
         if "email" in body:
             user1.email = body["email"]
+        if "lastname" not in body:
+            raise APIException("You need to specify Last Name", status_code=400)
+        if "firstname" not in body:
+            raise APIException("You need to specify the First Name", status_code=400)
+        if "middlename" not in body:
+            raise APIException("You need to specify the Middle Name or N/A", status_code=400)
+        if "sex" not in body:
+            raise APIException("You need to specify gender", status_code=400)
+        if "dateOfBirth" not in body:
+            raise APIException("You need to specify your date of birth", status_code=400)
+        if "countryOfBirth" not in body:
+            raise APIException("You need to specify your country of birth or N/A", status_code=400)
+        if "citizenship" not in body:
+            raise APIException("You need to specify your citizenship or N/A", status_code=400)
+        if "US_Address" not in body:
+            raise APIException("You need to specify your address", status_code=400)
+        if "city" not in body:
+            raise APIException("You need to specify the city", status_code=400)
+        if "state" not in body:
+            raise APIException("You need to specify state", status_code=400)
+        if "zip" not in body:
+            raise APIException("You need to specify zip code", status_code=400)
+        if "mobile" not in body:
+            raise APIException("You need to specify your phone number or N/A", status_code=400)
+
+
         db.session.commit()
 
         return jsonify(user1.serialize()), 200
@@ -135,8 +160,6 @@ def handle_spouse():
 
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
-        if "username" not in body:
-            raise APIException("You need to specify the username", status_code=400)
         if "email" not in body:
             raise APIException("You need to specify the email", status_code=400)
         if "lastname" not in body:
@@ -149,7 +172,7 @@ def handle_spouse():
             raise APIException("You need to specify sex", status_code=400)
         if "dateOfBirth" not in body:
             raise APIException("You need to specify your date of birth", status_code=400)
-        if "countyOfBirth" not in body:
+        if "countryOfBirth" not in body:
             raise APIException("You need to specify your country of birth or N/A", status_code=400)
         if "citizenship" not in body:
             raise APIException("You need to specify your citizenship or N/A", status_code=400)
@@ -161,10 +184,12 @@ def handle_spouse():
             raise APIException("You need to specify state", status_code=400)
         if "zip" not in body:
             raise APIException("You need to specify zip code", status_code=400)
+        if "mobile" not in body:
+            raise APIException("You need to specify your phone number or N/A", status_code=400)
 
 
-        user1 = Spouse(spouse_id=body['spouse_id'], email=body['email'])
-        db.session.add(user1)
+        spouse1 = Spouse(email=body["email"], lastname=body["lastname"], firstname=body["firstname"], middlename=body["middlename"], sex=body["sex"], dateOfBirth=body["dateOfBirth"], countryOfBirth=body["countryOfBirth"], citizenship=body["citizenship"], US_Address=body["US_Address"], city=body["city"], state=body["state"], zip=body["zip"], mobile=body["mobile"])
+        db.session.add(spouse1)
         db.session.commit()
         return "ok", 200
 
@@ -188,14 +213,15 @@ def get_single_spouse(spouse_id):
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
 
-    user1 = Spouse.query.get(spouse_id)
-    if user1 is None:
+    spouse1 = Spouse.query.get(spouse_id)
+    if spouse1 is None:
         raise APIException('User not found', status_code=404)
+
 
     if body is None:
         raise APIException("You need to specify the request body as a json object", status_code=400)
-    if "email" not in body:
-        raise APIException("You need to specify the email", status_code=400)
+    if "email" in body:
+        spouse1.email = body["email"]
     if "lastname" not in body:
         raise APIException("You need to specify Last Name", status_code=400)
     if "firstname" not in body:
@@ -206,7 +232,7 @@ def get_single_spouse(spouse_id):
         raise APIException("You need to specify sex", status_code=400)
     if "dateOfBirth" not in body:
         raise APIException("You need to specify your date of birth", status_code=400)
-    if "countyOfBirth" not in body:
+    if "countryOfBirth" not in body:
         raise APIException("You need to specify your country of birth or N/A", status_code=400)
     if "citizenship" not in body:
         raise APIException("You need to specify your citizenship or N/A", status_code=400)
@@ -218,28 +244,168 @@ def get_single_spouse(spouse_id):
         raise APIException("You need to specify state", status_code=400)
     if "zip" not in body:
         raise APIException("You need to specify zip code", status_code=400)
-    db.session.commit()
+    if "mobile" not in body:
+        raise APIException("You need to specify your phone number or N/A", status_code=400)
 
-    return jsonify(user1.serialize()), 200
+    db.session.commit()
+    return jsonify(spouse1.serialize()), 200
 
     # GET request
     if request.method == 'GET':
-        user1 = Spouse.query.get(spouse_id)
-        if user1 is None:
+        spouse1 = Spouse.query.get(spouse_id)
+        if spouse1 is None:
             raise APIException('User not found', status_code=404)
-        return jsonify(user1.serialize()), 200
+        return jsonify(spouse1.serialize()), 200
 
     # DELETE request
     if request.method == 'DELETE':
-        user1 = Spouse.query.get(spouse_id)
-        if user1 is None:
-            raise APIException('User not found', status_code=404)
-        db.session.delete(user1)
+        spouse1 = Spouse.query.get(spouse_id)
+        if spouse1 is None:
+            raise APIException('Spouse not found', status_code=404)
+        db.session.delete(spouse1)
         db.session.commit()
         return "ok", 200
 
     return "Invalid Method", 404
 
+
+@app.route('/application', methods=['POST', 'GET'])
+def handle_application():
+    """
+    Create application and retrieve all applications
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if "application_name" not in body:
+            raise APIException("You need to specify the application", status_code=400)
+
+        apps1 = Application(application_name=body['application_name'])
+        db.session.add(apps1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_applications = Application.query.all()
+        all_applications = list(map(lambda x: x.serialize(), all_applications))
+        return jsonify(all_applications), 200
+
+    return "Invalid Method", 404
+
+
+@app.route('/application/<int:application_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_application(application_id):
+    """
+    Single application
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        apps1 = Application.query.get(application_id)
+        if apps1 is None:
+            raise APIException('User not found', status_code=404)
+
+        if "application_name" in body:
+            apps1.application_name = body["application_name"]
+
+        db.session.commit()
+        return jsonify(apps1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        apps1 = Application.query.get(application_id)
+        if apps1 is None:
+            raise APIException('Application not found', status_code=404)
+        return jsonify(apps1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        apps1 = Application.query.get(application_id)
+        if apps1 is None:
+            raise APIException('Application not found', status_code=404)
+        db.session.delete(apps1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+@app.route('/forms', methods=['POST', 'GET'])
+def handle_forms():
+    """
+    Create forms and retrieve all forms
+    """
+
+    # POST request
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        if "forms_name" not in body:
+            raise APIException("You need to specify the forms_name", status_code=400)
+
+        form1 = Forms(forms_name=body['forms_name'])
+        db.session.add(form1)
+        db.session.commit()
+        return "ok", 200
+
+    # GET request
+    if request.method == 'GET':
+        all_forms = Forms.query.all()
+        all_forms = list(map(lambda x: x.serialize(), all_forms))
+        return jsonify(all_forms), 200
+
+    return "Invalid Method", 404
+
+@app.route('/forms/<int:forms_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_forms(forms_id):
+    """
+    Single forms
+    """
+
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        form1 = Forms.query.get(forms_id)
+        if form1 is None:
+            raise APIException('Information not found', status_code=404)
+
+        if "forms_name" in body:
+            form1.forms_name = body["forms_name"]
+
+        db.session.commit()
+
+        return jsonify(form1.serialize()), 200
+
+    # GET request
+    if request.method == 'GET':
+        form1 = Forms.query.get(forms_id)
+        if form1 is None:
+            raise APIException('Informaion not found', status_code=404)
+        return jsonify(form1.serialize()), 200
+
+    # DELETE request
+    if request.method == 'DELETE':
+        form1 = Forms.query.get(forms_id)
+        if form1 is None:
+            raise APIException('Information not found', status_code=404)
+        db.session.delete(form1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,13 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# from SQLAlchemy import relationship, ForeignKey
+# from flask_sqlalchemy import
+
+# from flask.ext.sqlalchemy import SQLAlchemy
+# from sqlalchemy.ext.declarative import SQLalchemy
+
 db = SQLAlchemy()
 
-
 class Person(db.Model):
-    # __tablename__ = "person"
-    # spouse = db.relationship("Spouse", uselist=False, back_populates="person")
-    # id = db.Column(db.Integer, primary_key=True)
-
+    __tablename__ = "person"
+    spouse = db.relationship("Spouse", uselist=False, back_populates="person")
+    id = db.Column(db.Integer, primary_key=True)
     person_id = db.Column(db.Integer, unique=True, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,7 +20,7 @@ class Person(db.Model):
     middlename = db.Column(db.String(120), unique=False, nullable=True)
     sex = db.Column(db.String(6), unique=False, nullable=True)
     dateOfBirth = db.Column(db.Date, unique=False, nullable=True)
-    countyOfBirth = db.Column(db.String(30), unique=False, nullable=True)
+    countryOfBirth = db.Column(db.String(30), unique=False, nullable=True)
     citizenship = db.Column(db.String(30), unique=False, nullable=True)
     US_Address = db.Column(db.String(120), unique=False, nullable=True)
     apartment = db.Column(db.String(120), unique=False, nullable=True)
@@ -24,53 +28,50 @@ class Person(db.Model):
     state = db.Column(db.String(20), unique=False, nullable=True)
     zip = db.Column(db.String(10), unique=False, nullable=True)
     mobile = db.Column(db.String(20), unique=False, nullable=True)
+    spouse_id = db.Column(db.Integer, unique=True, nullable=False)
 
-def __repr__(self):
-    return '<Person %r>' % self.person_id
+    def __repr__(self):
+        return '<Person %r>' % self.person_id
 
-def serialize(self):
-    return {
-        "id": self.id,
-        "username": self.username,
-        "email": self.email,
-        "lastname": self.lastname,
-        "firstname": self.firstname,
-        "middlename": self.middlename,
-        "sex": self.sex,
-        "dateOfBirth": self.dateOfBirth,
-        "countyOfBirth": self.countyOfBirth,
-        "citizenship": self.citizenship,
-        "US_Address": self.US_Address,
-        "apartment": self.apartment,
-        "city": self.city,
-        "state": self.state,
-        "zip": self.zip,
-        "mobile": self.mobile,
-        "person_id": self.person_id,
-        "spouse_id": self.spouse_id,
-        "application_id": self.application_id
-    }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "lastname": self.lastname,
+            "firstname": self.firstname,
+            "middlename": self.middlename,
+            "sex": self.sex,
+            "dateOfBirth": self.dateOfBirth,
+            "countryOfBirth": self.countryOfBirth,
+            "citizenship": self.citizenship,
+            "US_Address": self.US_Address,
+            "apartment": self.apartment,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "mobile": self.mobile
+        }
 
-    class Spouse(db.Model):
-        # __tablename__ = 'spouse'
-        # person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
-        # person = db.relationship("Person", back_populates="spouse")
-        id = db.Column(db.Integer, primary_key=True)
-        email = db.Column(db.String(120), unique=True, nullable=False)
-        lastname = db.Column(db.String(120), unique=False, nullable=True)
-        firstname = db.Column(db.String(120), unique=False, nullable=True)
-        middlename = db.Column(db.String(120), unique=False, nullable=True)
-        sex = db.Column(db.String(6), unique=False, nullable=True)
-        dateOfBirth = db.Column(db.Date, unique=False, nullable=True)
-        countyOfBirth = db.Column(db.String(30), unique=False, nullable=True)
-        citizenship = db.Column(db.String(30), unique=False, nullable=True)
-        US_Address = db.Column(db.String(120), unique=False, nullable=True)
-        apartment = db.Column(db.String(120), unique=False, nullable=True)
-        city = db.Column(db.String(30), unique=False, nullable=True)
-        state = db.Column(db.String(20), unique=False, nullable=True)
-        zip = db.Column(db.String(10), unique=False, nullable=True)
-        mobile = db.Column(db.String(20), unique=False, nullable=True)
-        spouse_id = db.Column(db.Integer(20), unique=True, nullable=True)
+class Spouse(db.Model):
+    __tablename__ = 'spouse'
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
+    person = db.relationship("Person", back_populates="spouse")
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    lastname = db.Column(db.String(120), unique=False, nullable=True)
+    firstname = db.Column(db.String(120), unique=False, nullable=True)
+    middlename = db.Column(db.String(120), unique=False, nullable=True)
+    sex = db.Column(db.String(6), unique=False, nullable=True)
+    dateOfBirth = db.Column(db.Date, unique=False, nullable=True)
+    countryOfBirth = db.Column(db.String(30), unique=False, nullable=True)
+    citizenship = db.Column(db.String(30), unique=False, nullable=True)
+    US_Address = db.Column(db.String(120), unique=False, nullable=True)
+    apartment = db.Column(db.String(120), unique=False, nullable=True)
+    city = db.Column(db.String(30), unique=False, nullable=True)
+    state = db.Column(db.String(20), unique=False, nullable=True)
+    zip = db.Column(db.String(10), unique=False, nullable=True)
+    mobile = db.Column(db.String(20), unique=False, nullable=True)
 
     def __repr__(self):
         return '<Spouse %r>' % self.spouse_id
@@ -84,48 +85,41 @@ def serialize(self):
             "middlename": self.middlename,
             "sex": self.sex,
             "dateOfBirth": self.dateOfBirth,
-            "countyOfBirth": self.countyOfBirth,
+            "countryOfBirth": self.countryOfBirth,
             "citizenship": self.citizenship,
             "US_Address": self.US_Address,
             "apartment": self.apartment,
             "city": self.city,
             "state": self.state,
             "zip": self.zip,
-            "mobile": self.mobile,
-            "spouse_id": self.spouse_id
+            "mobile": self.mobile
         }
 
-
-    class Application(db.Model):
-        # __tablename__ = 'application'
-        id = db.Column(db.Integer, primary_key=True)
-        # children = db.relationship("Forms")
-        application_name = db.Column(db.String(80), unique=True, nullable=False)
-        application_id = db.Column(db.String(120), unique=True, nullable=False)
-
+class Application(db.Model):
+    __tablename__ = 'application'
+    id = db.Column(db.Integer, primary_key=True)
+    children = db.relationship("Forms")
+    application_name = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self): return '<Application %r>' % self.application_id
 
     def serialize(self):
         return {
             "id": self.id,
-            "application_name": self.application_name,
-            "application_id": self.application_id
+            "application_name": self.application_name
         }
 
+class Forms(db.Model):
+    __tablename__ = "forms"
+    id = db.Column(db.Integer, primary_key=True)
 
-    class Forms(db.Model):
-        # __tablename__ = "forms"
-        id = db.Column(db.Integer, primary_key=True)
-        # application_id = db.Column(db.Integer, db.ForeignKey("application.id"))
-        forms_id = db.Column(db.String(120), unique=True, nullable=False)
-        forms_name = db.Column(db.String(120), unique=False, nullable=False)
+    application_id = db.Column(db.Integer, db.ForeignKey("application.id"))
+    forms_name = db.Column(db.String(120), unique=False, nullable=False)
 
     def __repr__(self): return '<Forms %r>' % self.forms_id
 
     def serialize(self):
         return {
             "id": self.id,
-            "forms_id": self.forms_id,
             "forms_name": self.forms_name
         }
